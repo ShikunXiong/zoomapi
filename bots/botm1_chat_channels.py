da4c214d201199f201d34dbdd62fc75db28ad933
ShikunXiong <shikun.xiong.work@gmail.com>
2020-04-03 04:55:32 -0700

    update botm1_chat_channels
    use command line tips to test APIs

--- bots/botm1_chat_channels.py
+++ bots/botm1_chat_channels.py
@@ -22,54 +22,76 @@ client = OAuthZoomClient(client_id, client_secret, redirect_url, browser_path)
 user_response = client.user.get(id='me')
 user = json.loads(user_response.content)
 print(user)
-print('---')
+print('\n')
 
 # ids below are used for test
 test_channel_id = "109ab13498c64fd5911a42be1076ea6b"
 my_channel_id = "e00a1405fc5a4dc0980fa3c6dfed5989"
 yl_channel_id = "c1e85cd0e22844baaaa9cb2bf55f7704"
 yl_member_id = "s4fjawkhrtcwcpvxbhxtqw"
+flag = True
 
-# list channel members
-response = json.loads(client.chat_channels.list_members(channelId=my_channel_id).content)
-print("--list channel members--")
-print(response)
-
-# create a channel
-response = json.loads(
-    client.chat_channels.create(name="create_test", type=1, members=[{"email": "aa@gmail.com"}]).content)
-print("--create a channel--")
-print(response)
-
-# get a channel
-client.chat_channels.get(channelId=test_channel_id)
-response = json.loads(client.chat_channels.get(channelId=test_channel_id).content)
-print("--get a channel--")
-print(response)
-
-# update a channel
-response = client.chat_channels.update(channelId=my_channel_id, name="new_name")
-print("update status code: " + str(response.status_code))
-
-# delete channel
-# response = json.loads(client.chat_channels.delete(channelId=test_channel_id).content)
-# print(response)
-
-# invite
-response = json.loads(
-    client.chat_channels.invite(channelId=my_channel_id, members=[{"email": "tjuwangyilin@163.com"}]).content)
-print("--invite response--")
-print(response)
-
-# remove
-response = client.chat_channels.remove(channelId=my_channel_id, memberId=yl_member_id)
-print("remove status code: " + str(response.status_code))
-
-# join
-response = json.loads(client.chat_channels.join(channelId=yl_channel_id).content)
-print("--join a channel--")
-print(response)
-
-# leave
-response = client.chat_channels.leave(channelId=yl_channel_id)
-print("leave status code: " + str(response.status_code))
+while flag:
+    print("Please test the functions following this sequence: 1-2-3-4-6-7-8-9-5-0")
+    print("---Options---")
+    print("1. list channel numbers")
+    print("2. creat a channel")
+    print("3. get a channel")
+    print("4. update a channel")
+    print("5. delete a channel")
+    print("6. invite a member to a channel")
+    print("7. remove a member from a channel")
+    print("8. join to a channel")
+    print("9. leave from a channel")
+    print("0. exit")
+    option = input("Enter a option number:")
+    if option == "1":
+        channel_id = input("input channel id(use 'e00a1405fc5a4dc0980fa3c6dfed5989' for test):")
+        response = json.loads(client.chat_channels.list_members(channelId=my_channel_id).content)
+        print(response)
+    elif option == "2":
+        channel_name = input("input channel name:")
+        email = input("input members' emails(separate with comma):")
+        email = email.split(",")
+        tmp = []
+        for e in email:
+            tmp.append({"email": e})
+        response = json.loads(client.chat_channels.create(name=channel_name, type=1, members=tmp).content)
+        print(response)
+    elif option == "3":
+        channel_id = input("input channel id(use 'e00a1405fc5a4dc0980fa3c6dfed5989' for test):")
+        response = json.loads(client.chat_channels.get(channelId=channel_id).content)
+        print(response)
+    elif option == "4":
+        channel_id = input("input channel id(use 'e00a1405fc5a4dc0980fa3c6dfed5989' for test):")
+        new_name = input("input a new name for channel:")
+        response = client.chat_channels.update(channelId=channel_id, name=new_name)
+        print("update status code: " + str(response.status_code))
+    elif option == "5":
+        channel_id = input("input channel id(use 'e00a1405fc5a4dc0980fa3c6dfed5989' for test):")
+        response = client.chat_channels.delete(channelId=channel_id)
+        print("delete status code: " + str(response.status_code))
+    elif option == "6":
+        channel_id = input("input channel id(use 'e00a1405fc5a4dc0980fa3c6dfed5989' for test):")
+        email = input("input members' emails(use 'tjuwangyilin@163.com' for test):")
+        email = email.split(",")
+        tmp = []
+        for e in email:
+            tmp.append({"email": e})
+        response = json.loads(client.chat_channels.invite(channelId=channel_id, members=tmp).content)
+        print(response)
+    elif option == "7":
+        channel_id = input("input channel id(use 'e00a1405fc5a4dc0980fa3c6dfed5989' for test):")
+        member_id = input("input member id(use 's4fjawkhrtcwcpvxbhxtqw' for test):")
+        response = client.chat_channels.remove(channelId=channel_id, memberId=member_id)
+        print("remove status code: " + str(response.status_code))
+    elif option == "8":
+        channel_id = input("input channel id(use 'c1e85cd0e22844baaaa9cb2bf55f7704' for test):")
+        response = json.loads(client.chat_channels.join(channelId=channel_id).content)
+        print(response)
+    elif option == "9":
+        channel_id = input("input channel id(use 'c1e85cd0e22844baaaa9cb2bf55f7704' for test):")
+        response = client.chat_channels.leave(channelId=yl_channel_id)
+        print("leave status code: " + str(response.status_code))
+    else:
+        flag = False
