afcfb6981de5cb9b25cb87f83596de36970cfa91
ShikunXiong <shikun.xiong.work@gmail.com>
2020-04-06 13:09:14 -0700

    update Throttled control

--- zoomapi/components/chat_channels.py
+++ zoomapi/components/chat_channels.py
@@ -3,53 +3,64 @@ import json
 
 from zoomapi import util
 from zoomapi.components import base
+from zoomapi.util import Throttled
 
 
 class ChatChannelsComponentV2(base.BaseComponent):
     """Component dealing with all chat channels related matters"""
 
+    @Throttled
     def list(self, **kwargs):
         return self.get_request("/chat/users/me/channels")
 
+    @Throttled
     def create(self, **kwargs):
         util.require_keys(kwargs, ["name", "type", "members"])
         return self.post_request("/chat/users/me/channels", data=kwargs)
 
+    @Throttled
     def get(self, **kwargs):
         util.require_keys(kwargs, "channelId")
         return self.get_request(
             "/chat/channels/{channelId}".format(channelId=kwargs.get("channelId")))
 
+    @Throttled
     def update(self, **kwargs):
         util.require_keys(kwargs, ["channelId", "name"])
         return self.patch_request(
             "/chat/channels/{channelId}".format(channelId=kwargs.get("channelId")), data=kwargs)
 
+    @Throttled
     def delete(self, **kwargs):
         util.require_keys(kwargs, "channelId")
         return self.delete_request(
             "/chat/channels/{channelId}".format(channelId=kwargs.get("channelId")))
 
+    @Throttled
     def list_members(self, **kwargs):
         util.require_keys(kwargs, "channelId")
         return self.get_request(
             "/chat/channels/{channelId}/members".format(channelId=kwargs.get("channelId")))
 
+    @Throttled
     def invite(self, **kwargs):
         util.require_keys(kwargs, ["channelId", "members"])
         return self.post_request(
             "/chat/channels/{channelId}/members".format(channelId=kwargs.get("channelId")), data=kwargs)
 
+    @Throttled
     def join(self, **kwargs):
         util.require_keys(kwargs, "channelId")
         return self.post_request(
             "/chat/channels/{channelId}/members/me".format(channelId=kwargs.get("channelId")))
 
+    @Throttled
     def leave(self, **kwargs):
         util.require_keys(kwargs, "channelId")
         return self.delete_request(
             "/chat/channels/{channelId}/members/me".format(channelId=kwargs.get("channelId")))
 
+    @Throttled
     def remove(self, **kwargs):
         util.require_keys(kwargs, ["channelId", "memberId"])
         return self.delete_request(
--- zoomapi/components/chat_messages.py
+++ zoomapi/components/chat_messages.py
@@ -2,25 +2,30 @@
 
 from zoomapi import util
 from zoomapi.components import base
+from zoomapi.util import Throttled
 
 
 class ChatMessagesComponentV2(base.BaseComponent):
     """Component dealing with all chat messages related matters"""
 
+    @Throttled
     def list(self, **kwargs):
         util.require_keys(kwargs, "user_id")
         return self.get_request(
                 "/chat/users/{}/messages".format(kwargs.get("user_id")), params=kwargs
         )
 
+    @Throttled
     def post(self, **kwargs):
         util.require_keys(kwargs, "message")
         return self.post_request("/chat/users/me/messages", data=kwargs)
 
+    @Throttled
     def delete(self, **kwargs):
         util.require_keys(kwargs, "messageId")
         return self.delete_request("/chat/users/me/messages/{}".format(kwargs.get("messageId")), params=kwargs)
 
+    @Throttled
     def update(self, messageID, **kwargs):
         util.require_keys(kwargs, "message")
         return self.put_request("/chat/users/me/messages/{}".format(messageID), data=kwargs)
